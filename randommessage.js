a9d4236b53015edb5737645547a674d4b46326fa
mohit5543 <mb4504613@gmail.com>
2024-04-08 14:00:50 +0530

    Random message

--- randommessage.js
+++ randommessage.js
@@ -1,17 +1,22 @@
 // Arrays of different parts of a message
-const greetings = ['Hello', 'Hi', 'Hey', 'Greetings'];
-const subjects = ['everyone', 'world', 'friends', 'team'];
-const verbs = ['is coding', 'loves programming', 'enjoys learning', 'creates cool projects'];
-const objects = ['JavaScript', 'Python', 'Java', 'C++'];
-
+const greetings = [
+    'You are awesome!', 'Keep pushing!','The future is yours to code!',
+  ];
+  
+  const subjects = [
+    'Anyone who',  'A programmer who','Those who learn to code',
+  ];
+  
+  const verbs = [
+    'can code',  'builds amazing things','solves problems','makes a difference',
+  ];
 // Function to generate a random message
 function generateRandomMessage() {
     const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
     const randomSubject = subjects[Math.floor(Math.random() * subjects.length)];
     const randomVerb = verbs[Math.floor(Math.random() * verbs.length)];
-    const randomObject = objects[Math.floor(Math.random() * objects.length)];
 
-    return `${randomGreeting}, ${randomSubject} ${randomVerb} with ${randomObject}!`;
+    return `${randomGreeting}, ${randomSubject} ${randomVerb} !`;
 }
 
 // Generate and log a random message
